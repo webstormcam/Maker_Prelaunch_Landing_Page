897f462cec7132457252c2c8dd1c647e1ff92dfb
Cameron Chandler <webstormcam@gmail.com>
2022-03-06 11:13:27 -0500

    Final adjustments

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function validateForm(){
     let x=document.getElementById('email').value;
+    let xStyle = document.getElementById('email')
     let text;
     if (x ===''){
         text=' Oops! Please add your email'
-    
+        xStyle.style ='border: 1px solid red;'
     } else if (x.includes('@')==false){
         text='Oops! Please check your email'
-    
+        xStyle.style ='border: 1px solid red;'
     }else if (x.length<5 && x.includes('@')===true){
         text='Oops! Please check your email'
-   
+        xStyle.style ='border: 1px solid red;'
     } else if (x.length>=6 && x.includes('@')===true){
         text=''
         document.getElementById("myForm").submit();
